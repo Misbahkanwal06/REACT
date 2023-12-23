63ca7e5396287d37a8ba3b39cbdc1f40993b433b
Misbahkanwal06 <misbahkan26@gmail.com>
2023-12-23 17:20:58 +0500

    Registration-Form-REACT

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import  RegistrationForm from "./assets/components/RegistrationForm";
+
+function App() {
+
+  return (
+    <>
+    < RegistrationForm/> 
+      
+    </>
+  )
+}
+
+export default App
